4497076e731c48483d4cbc51e952378c3d9b0bdc
Rueben Ramirez <ruebenramirez@gmail.com>
2018-02-09 08:39:59 -0600

    best trackpoint settings I've found so far
    * source: this reddit comment:
      https://www.reddit.com/r/thinkpad/comments/6hv7zf/any_linux_users_able_to_adjust_trackpoint/dj3e612/

--- bin/configure-trackpad-palm-detection.sh
+++ bin/configure-trackpad-palm-detection.sh
@@ -46,12 +46,12 @@ elif [[ $(hostname -f) == "rramirez-ThinkPad-T470" ]]; then
     xinput --enable "$TRACKPOINT"
     # xinput --list-props "$TRACKPOINT"
 
-    echo 75 | sudo tee /sys/devices/platform/i8042/serio1/serio2/speed
-    echo 85 | sudo tee /sys/devices/platform/i8042/serio1/serio2/sensitivity
-    echo "trackpoint speed: "
-    cat /sys/devices/platform/i8042/serio1/serio2/speed
-    echo "trackpoint sensitivity: "
-    cat /sys/devices/platform/i8042/serio1/serio2/sensitivity
+    # best trackpoint settings I've found:
+    # source: https://www.reddit.com/r/thinkpad/comments/6hv7zf/any_linux_users_able_to_adjust_trackpoint/dj3e612/
+    echo 191 | sudo tee /sys/devices/platform/i8042/serio1/serio2/sensitivity
+    echo 200 | sudo tee /sys/devices/platform/i8042/serio1/serio2/rate
+    echo 255 | sudo tee /sys/devices/platform/i8042/serio1/serio2/speed
+    echo 5 | sudo tee /sys/devices/platform/i8042/serio1/serio2/inertia
 else
 
     # older thinkpad trackpad config
