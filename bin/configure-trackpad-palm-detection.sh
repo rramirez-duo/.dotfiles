8170ebb11d10c2a94829e620cd5d5d775c6f4cb4
Rueben Ramirez <ruebenramirez@gmail.com>
2018-06-22 13:25:47 -0500

    dialing trackpoint and touchpad configuration

--- bin/configure-trackpad-palm-detection.sh
+++ bin/configure-trackpad-palm-detection.sh
@@ -22,7 +22,7 @@ elif [[ $(hostname -f) == "tpx1" ]]; then
     TOUCHPAD="SynPS/2 Synaptics TouchPad"
     xinput --enable "$TOUCHPAD"
     xinput set-prop "$TOUCHPAD" "libinput Natural Scrolling Enabled" 1
-    xinput set-prop "$TOUCHPAD" "libinput Tapping Enabled" 0
+    xinput set-prop "$TOUCHPAD" "libinput Tapping Enabled" 1
     xinput set-prop "$TOUCHPAD" "libinput Click Method Enabled" 0, 1
     xinput --disable "$TOUCHPAD"
 
@@ -30,7 +30,7 @@ elif [[ $(hostname -f) == "tpx1" ]]; then
     xinput --enable "$TRACKPOINT"
     xinput --list-props "$TRACKPOINT"
     xinput set-prop "$TRACKPOINT" "libinput Accel Profile Enabled" 1, 0
-    xinput --set-prop "$TRACKPOINT" "libinput Accel Speed" -.05
+    xinput --set-prop "$TRACKPOINT" "libinput Accel Speed" 0
 
 
 else
